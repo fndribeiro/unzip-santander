021210cd14ca3a315cc1d2fa7aa9f5d5dd4adf8f
fndribeiro <fndribeiro@hotmail.com>
2020-05-14 19:16:14 -0300

    removidas credenciais de EmailConfig.

--- src/br/com/ribeiro/fernando/unzipsantander/EmailConfig.java
+++ src/br/com/ribeiro/fernando/unzipsantander/EmailConfig.java
@@ -2,13 +2,13 @@ package br.com.ribeiro.fernando.unzipsantander;
 
 public class EmailConfig {
 	
-	static final String to = "awilson@lemontech.com.br";
-    static final String toCc = "equipe.implantacao@lemontech.com.br";
-    static final String from = "implantacao@lemontech.com.br";
-    static final String username = "implantacao@lemontech.com.br";
-    static final String password = "itwhljctbiuiihuf";
-    static final String subject = "Arquivos Santander";
-    static final String bodyText = "Envio automatico dos arquivos Santander.";
-    static final String searchTerm = "portalrh@santander.com.br";
+	static final String to = "";
+    static final String toCc = "";
+    static final String from = "";
+    static final String username = "";
+    static final String password = "";
+    static final String subject = "";
+    static final String bodyText = "";
+    static final String searchTerm = "";
     
 }
